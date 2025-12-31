0f60fd2a947546024b1719488f1ef82aa7905677
feijaoozin <gonardo1234@gmail.com>
2025-12-31 11:06:19 -0300

    feat: mudando as labels do countdown para pt-BR

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,3 +1,48 @@
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import { motion } from "framer-motion";
+
+// function getTimeLeft() {
+//   const now = new Date();
+//   const nextYear = new Date(now.getFullYear() + 1, 0, 1);
+//   const diff = nextYear.getTime() - now.getTime();
+
+//   return {
+//     days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+//     hours: Math.floor((diff / (1000 * 60 * 60)) % 24),
+//     minutes: Math.floor((diff / (1000 * 60)) % 60),
+//     seconds: Math.floor((diff / 1000) % 60),
+//   };
+// }
+
+// export function Countdown() {
+//   const [time, setTime] = useState(getTimeLeft());
+
+//   useEffect(() => {
+//     const timer = setInterval(() => setTime(getTimeLeft()), 1000);
+//     return () => clearInterval(timer);
+//   }, []);
+
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0, y: 20 }}
+//       animate={{ opacity: 1, y: 0 }}
+//       transition={{ delay: 0.3 }}
+//       className="flex gap-6 text-center mt-6 mb justify-center"
+//     >
+//       {Object.entries(time).map(([label, value]) => (
+//         <div key={label}>
+//           <p className="text-3xl font-bold">{value}</p>
+//           <span className="text-sm text-muted-foreground uppercase">
+//             {label}
+//           </span>
+//         </div>
+//       ))}
+//     </motion.div>
+//   );
+// }
+
 "use client";
 
 import { useEffect, useState } from "react";
@@ -16,6 +61,13 @@ function getTimeLeft() {
   };
 }
 
+const labelsPT = {
+  days: "dias",
+  hours: "horas",
+  minutes: "minutos",
+  seconds: "segundos",
+} as const;
+
 export function Countdown() {
   const [time, setTime] = useState(getTimeLeft());
 
@@ -29,13 +81,13 @@ export function Countdown() {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.3 }}
-      className="flex gap-6 text-center mt-6 mb justify-center"
+      className="flex gap-6 text-center mt-6 justify-center"
     >
       {Object.entries(time).map(([label, value]) => (
         <div key={label}>
           <p className="text-3xl font-bold">{value}</p>
           <span className="text-sm text-muted-foreground uppercase">
-            {label}
+            {labelsPT[label as keyof typeof labelsPT]}
           </span>
         </div>
       ))}
