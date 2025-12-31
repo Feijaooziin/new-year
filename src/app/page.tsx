07f4dacbedf74d31e3a5fe91ac89fc2c63f4f0ba
feijaoozin <gonardo1234@gmail.com>
2025-12-31 11:11:37 -0300

    fix: arrumando animações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,45 +91,43 @@ export default function Home() {
         animate="visible"
         className="px-6 py-8 text-center text-xl leading-relaxed text-zinc-400 space-y-4"
       >
-        <motion.h1 className="text-3xl font-bold text-zinc-200">
-          Oi Gata!
-        </motion.h1>
+        <h1 className="text-3xl font-bold text-zinc-200">Oi Gata!</h1>
 
-        <motion.p>
+        <p>
           Chegamos ao fim de mais um ano, e quando olho para trás, vejo o quanto
           ele foi intenso para nós. Não foi um ano fácil — tivemos desafios,
           dias pesados, incertezas e momentos em que tudo parecia mais difícil
           do que deveria ser. Mas, mesmo assim, seguimos em frente. Juntos.
-        </motion.p>
+        </p>
 
-        <motion.p>
+        <p>
           Aprendemos que superar não é nunca cair, mas escolher levantar lado a
           lado. Em cada dificuldade, você esteve ali, sendo apoio, força e amor.
           E foi assim, passo a passo, que conseguimos transformar um ano difícil
           em um ano de crescimento.
-        </motion.p>
+        </p>
 
-        <motion.p>
+        <p>
           Construímos muitas coisas: sonhos mais fortes, uma parceria ainda mais
           sólida, aprendizados que vamos levar para a vida inteira e, acima de
           tudo, um amor mais maduro e verdadeiro. Tudo o que conquistamos só fez
           sentido porque fizemos juntos.
-        </motion.p>
+        </p>
 
-        <motion.p>
+        <p>
           Sou imensamente grato por você, pela nossa história e por não soltar
           minha mão nem nos momentos mais duros (Lá ele kkkk).
-        </motion.p>
+        </p>
 
-        <motion.p>
+        <p>
           Que o próximo ano venha com mais leveza, mais sorrisos e novos sonhos
           — e que, aconteça o que acontecer, continuemos assim: unidos, fortes e
           cheios de amor.
-        </motion.p>
+        </p>
 
-        <motion.p className="font-medium text-zinc-300">
+        <p className="font-medium text-zinc-300">
           Te amo. Que venham novos começos, juntos. ✨❤️
-        </motion.p>
+        </p>
       </motion.footer>
     </main>
   );
